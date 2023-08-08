65bcc80b459a55034c241a43fcc31f85f17d41b8
weeksontheweb <andy@weeksontheweb.co.uk>
2023-08-08 22:46:34 +0100

    First commit.

---
+++ AccuWeatherWorkService/Worker.cs
@@ -0,0 +1,62 @@
+using AccuWeatherWorkService.Models;
+using Newtonsoft.Json;
+
+
+namespace AccuWeatherWorkService;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    private readonly string apiKey;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+        apiKey = System.Environment.GetEnvironmentVariable("AccuWeatherApiKey");
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+                
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                /*
+                using (var httpClient = new HttpClient())
+                {
+                   using (HttpResponseMessage response = await httpClient.GetAsync("http://dataservice.accuweather.com/forecasts/v1/daily/5day/55488?apikey=" + apiKey,stoppingToken))
+                   {
+                        string apiResponse = await response.Content.ReadAsStringAsync(stoppingToken);
+
+                        DailyConditions dailyConditions = JsonConvert.DeserializeObject<DailyConditions>(apiResponse);
+
+                        Console.WriteLine("Rate: " + dailyConditions.Headline.Severity);
+                   }
+                }
+                */
+                using (var httpClient = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await httpClient.GetAsync(
+                               "http://dataservice.accuweather.com/currentconditions/v1/1113694?apikey=" + apiKey + "&details=true",
+                               stoppingToken))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync(stoppingToken);
+
+                        List<CurrentConditions> currentConditions = JsonConvert.DeserializeObject<List<CurrentConditions>>(apiResponse);
+
+                        Console.WriteLine("Rate: " + currentConditions[0].LocalObservationDateTime);
+                    }
+                }
+
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error found is - " + ex.ToString());    
+        }
+    }
+}
