404a3d7b647c46b60169a4b4792aa3d414b49f29
weeksontheweb <andy@weeksontheweb.co.uk>
2023-08-15 21:08:26 +0100

    Added project and implemented read from RabbitMQ

---
+++ AccuWeatherConsumerToDB/Services/RabbitMQConsumer.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+
+namespace AccuWeatherConsumerToDB.Services;
+
+public class RabbitMQConsumer
+{
+    private ConnectionFactory factory;
+    private string messageQueueEndPoint;
+    private string messageQueueUserName;
+    private string messageQueuePassword;
+    
+    public RabbitMQConsumer()
+    {
+        messageQueueEndPoint = System.Environment.GetEnvironmentVariable("RabbitMQServiceEndPoint");
+        messageQueueUserName = System.Environment.GetEnvironmentVariable("RabbitMQServiceUserName");
+        messageQueuePassword = System.Environment.GetEnvironmentVariable("RabbitMQServicePassword");
+
+        //ConnectionFactory factory = new();
+        factory = new();
+        factory.Uri = new Uri("amqp://" + messageQueueUserName 
+                                        +":"+ messageQueuePassword + "@" + messageQueueEndPoint);
+    
+        factory.ClientProvidedName = "AccuWeather Consumer to DB";
+        
+    }
+
+    public int ReadMessageFromQueue()
+    {
+        IConnection cnn = factory.CreateConnection();
+
+        IModel channel = cnn.CreateModel();
+
+        string exchangeName = "DemoExchange";
+        string routingKey = "demo-routing-key";
+        string queueName = "DemoQueue";
+
+        channel.ExchangeDeclare(exchangeName,ExchangeType.Direct);
+        channel.QueueDeclare(queueName, false, false, false, null);
+        channel.QueueBind(queueName,exchangeName,routingKey,null);
+        channel.BasicQos(0,1,false);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (sender, args) =>
+        {
+            var body = args.Body.ToArray();
+
+            string message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"Message received: {message}");
+
+            channel.BasicAck(args.DeliveryTag, false);
+
+        };
+
+        string consumerTag = channel.BasicConsume(queueName,false,consumer);
+
+        Console.ReadLine(); 
+
+        channel.BasicCancel(consumerTag);
+
+        channel.Close();
+        cnn.Close();
+
+        return 1;
+    }
+}
---
+++ AccuWeatherConsumerToDB/Worker.cs
@@ -0,0 +1,25 @@
+using AccuWeatherConsumerToDB.Services;
+
+namespace AccuWeatherConsumerToDB;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            RabbitMQConsumer messageQueue = new RabbitMQConsumer();
+            messageQueue.ReadMessageFromQueue();
+            
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
